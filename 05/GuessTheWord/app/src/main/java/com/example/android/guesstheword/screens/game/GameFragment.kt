c063a789e39c0b6146a7d49a21c9257a78d63f92
lakshmi-warrier <lakshmi0105warrier@gmail.com>
2021-05-17 17:32:08 +0530

    Guess the word - ViewModel and LiveData done

--- 05/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ 05/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -53,23 +53,14 @@ class GameFragment : Fragment() {
 
         Log.i("GameFragment", "Called ViewModelProvider.get")
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
+        binding.gameViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
-        })
 
-        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
-            binding.wordText.text = newWord
-        })
 
         viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer<Boolean> { hasFinished ->
             if (hasFinished) gameFinished()
         })
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
-        binding.endGameButton.setOnClickListener { onEndGame() }
-
-
         return binding.root
 
     }
@@ -77,24 +68,6 @@ class GameFragment : Fragment() {
 
     /** Methods for button click handlers **/
 
-    private fun onSkip() {
-        viewModel.onSkip()
-
-    }
-
-    private fun onCorrect() {
-        viewModel.onCorrect()
-
-    }
-
-
-    /** Methods for updating the UI **/
-
-
-    private fun onEndGame() {
-        gameFinished()
-
-    }
 
     private fun gameFinished() {
         Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
